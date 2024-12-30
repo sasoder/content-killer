581082cabfa97118cb144f6d477f19f4f5351aed
Samuel Söderberg <samuelsoderberg99@gmail.com>
2024-12-30 11:29:08 +0100

    yt-dl metadata fixed

--- server/src/lib/generateMetadata.ts
+++ server/src/lib/generateMetadata.ts
@@ -1,24 +1,35 @@
 import { VideoMetadata } from '@shared/types/api/schema';
 import youtubeDl, { create } from 'youtube-dl-exec';
-
-require('dotenv').config();
-
-const ytDl = create(process.env.YT_DLP_PATH || 'yt-dlp');
+import 'dotenv/config';
 
 export const generateMetadata = async (url: string): Promise<Partial<VideoMetadata>> => {
 	try {
-		const result = await ytDl(url, {
-			dumpSingleJson: true,
-			noCheckCertificates: true,
-			noWarnings: true,
-			preferFreeFormats: true,
-			addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
-		});
-
-		return {
-			title: result.title as string,
-			duration: result.duration_string as string,
-		};
+		//check if there is a youtube-dl binary specified in the environment variables
+		if (process.env.YT_DLP_PATH) {
+			const result = (await create(process.env.YT_DLP_PATH as string)(url, {
+				dumpSingleJson: true,
+				noCheckCertificates: true,
+				noWarnings: true,
+				preferFreeFormats: true,
+				addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
+			})) as any;
+			return {
+				title: result.title,
+				duration: result.duration_string,
+			};
+		} else {
+			const result = (await youtubeDl(url, {
+				dumpSingleJson: true,
+				noCheckCertificates: true,
+				noWarnings: true,
+				preferFreeFormats: true,
+				addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
+			})) as any;
+			return {
+				title: result.title,
+				duration: result.duration_string,
+			};
+		}
 	} catch (error) {
 		console.error('Error fetching video metadata:', error);
 		if (error instanceof Error) {
