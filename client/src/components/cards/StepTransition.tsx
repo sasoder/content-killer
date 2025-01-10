9f8603b1c1ec8f41b158937471017505c6520cde
Samuel Söderberg <samuelsoderberg99@gmail.com>
2025-01-10 16:36:20 +0100

    transition icon width

--- client/src/components/cards/StepTransition.tsx
+++ client/src/components/cards/StepTransition.tsx
@@ -11,7 +11,7 @@ type StepTransitionProps = {
 export default function StepTransition({ data, jsonEditorTitle, onUpdate }: StepTransitionProps) {
 	return (
 		<div className='flex flex-col items-center justify-center'>
-			<Icons.moveRight className='h-8 w-8' strokeWidth={0.8} color='gray' />
+			<Icons.moveRight className='h-8 w-8' strokeWidth={1.3} color='gray' />
 			<div className='absolute pt-24'>
 				{data && data.length > 0 && <JsonEditor title={jsonEditorTitle} data={data} onUpdate={onUpdate} />}
 			</div>
